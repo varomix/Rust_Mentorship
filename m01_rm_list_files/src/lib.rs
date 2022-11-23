2e534a12ff7be12fbaf2dcc2978b4c17397cec22
varomix <varomix@gmail.com>
2022-11-22 21:31:32 -0700

    added sequence step property

--- m01_rm_list_files/src/lib.rs
+++ m01_rm_list_files/src/lib.rs
@@ -1,12 +1,33 @@
 pub fn frame_set(pat: &str) -> Vec<i32> {
     let mut result = Vec::new();
-    if pat.contains("-") {
-        let frm_range: Vec<&str> = pat.split('-').collect();
-        let start: i32 = frm_range[0].parse().unwrap();
-        let end: i32 = frm_range[1].parse().unwrap();
-        for n in start..=end {
-            result.push(n);
+
+    let mut step: i32 = 1;
+    let mut frm_range: Vec<&str>;
+    let mut start: i32 = 0;
+    let mut end: i32 = 10;
+    let mut raw_step: Vec<&str> = Default::default();
+
+    if pat.contains(':') {
+        raw_step = pat.split(':').collect();
+        step = raw_step[1].parse().unwrap();
+    }
+    // there is no step defined
+    if raw_step.is_empty() {
+        if pat.contains('-') {
+            frm_range = pat.split('-').collect();
+            start = frm_range[0].parse().unwrap();
+            end = frm_range[1].parse().unwrap();
         }
+        // step is defined
+    } else {
+        if raw_step[0].contains('-') {
+            frm_range = raw_step[0].split('-').collect();
+            start = frm_range[0].parse().unwrap();
+            end = frm_range[1].parse().unwrap();
+        }
+    }
+    for n in (start..=end).step_by(step as usize) {
+        result.push(n);
     }
     return result;
 }
--- m01_rm_list_files/src/main.rs
+++ m01_rm_list_files/src/main.rs
@@ -1,6 +1,9 @@
 use m01_rm_list_files::frame_set;
 
 fn main() {
-    let seq = frame_set("1-50");
+    let seq = frame_set("1-10");
     println!("{:?}", seq);
+
+    let seq2 = frame_set("1-100:8");
+    println!("{:?}", seq2);
 }
