7e39ae25987790151ac88e37f37bd41f3521a844
varomix <varomix@gmail.com>
2022-11-23 22:50:45 -0700

    Added File sequence generator

--- m01_rm_list_files/src/lib.rs
+++ m01_rm_list_files/src/lib.rs
@@ -1,8 +1,10 @@
-pub fn frame_set(pat: &str) -> Vec<i32> {
-    let mut result = Vec::new();
+use regex::Regex;
+
+pub fn frame_set(pat: &str) -> Result<Vec<i32>, String> {
+    let mut result: Vec<i32> = Vec::new();
 
     let mut step: i32 = 1;
-    let mut frm_range: Vec<&str>;
+    let frm_range: Vec<&str>;
     let mut start: i32 = 0;
     let mut end: i32 = 10;
     let mut raw_step: Vec<&str> = Default::default();
@@ -29,5 +31,25 @@ pub fn frame_set(pat: &str) -> Vec<i32> {
     for n in (start..=end).step_by(step as usize) {
         result.push(n);
     }
-    return result;
+    Ok(result)
+}
+
+pub fn file_sequence(pat: &str) -> Result<Vec<String>, String> {
+    let mut result: Vec<String> = Vec::new();
+
+    let re = Regex::new(r"\.").unwrap();
+    let split_pat: Vec<&str> = re.split(pat).collect();
+    let path = split_pat[0];
+    let range_exp = split_pat[1];
+    let extension = split_pat[2];
+
+    let start_end: Vec<&str> = range_exp.split("-").collect();
+    let start: i32 = start_end[0].parse().unwrap();
+    let end: i32 = start_end[1][0..start_end[1].len() - 1].parse().unwrap();
+    for n in start..=end {
+        let out = format!("{}.{:04}.{}", path, n, extension);
+        result.push(out);
+    }
+
+    Ok(result)
 }
--- m01_rm_list_files/src/main.rs
+++ m01_rm_list_files/src/main.rs
@@ -1,9 +1,19 @@
-use m01_rm_list_files::frame_set;
+use m01_rm_list_files::{file_sequence, frame_set};
+use std::fmt::format;
 
 fn main() {
     let seq = frame_set("1-10");
-    println!("{:?}", seq);
+    println!("Frame Set \"1-10\"    : {:?}", seq.unwrap());
 
     let seq2 = frame_set("1-100:8");
-    println!("{:?}", seq2);
+    println!("Frame Set \"1-100:8\" : {:?}", seq2.as_deref().unwrap());
+
+    println!("{}", seq2.as_ref().unwrap()[0]);
+    println!(
+        "{}",
+        seq2.as_ref().unwrap()[seq2.as_ref().unwrap().len() - 2]
+    );
+
+    let file_seq = file_sequence("/foo/bar.1-20#.exr");
+    println!("File Sequence: {:?}", file_seq.as_ref().unwrap());
 }
